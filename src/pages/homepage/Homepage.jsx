78aab4ee28ef21c616c101a6a35e59a69af09ee8
Roman Medvedev <roman.medvedev77@gmail.com>
2022-02-02 23:01:12 -0500

    add menu items components

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Directory from "../../components/directory/Directory";
+import MenuItem from "../../components/menu-item/Menu-item";
+import "./Homepage.scss";
+
+export const Homepage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
