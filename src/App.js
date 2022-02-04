8fb975cdc3bf057f93997d22e03cbc2fdaa6e257
Roman Medvedev <roman.medvedev77@gmail.com>
2022-02-04 11:58:37 -0500

    add higher order component withRouter

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/Hats' component={HatsPage} />
+        <Route path='/shop/hats' component={HatsPage} />
       </Switch>
     </div>
   );
