a33645a5f351fb00a2d3417cb176cb9956823ce3
Roman Medvedev <roman.medvedev77@gmail.com>
2022-03-29 16:42:17 -0400

    add Shop component

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,27 +1,43 @@
+// import React from "react";
+// import { CollectionPreview } from "../../components/collection-preview/Collection-preview.jsx";
+
+// import SHOP_DATA from "./shop.data.js";
+
 import React from "react";
-import { CollectionPreview } from "../../components/collection-preview/Collection-preview.jsx";
+import SHOP_DATA from "../../shop-data.json";
 
-import SHOP_DATA from "./shop.data.js";
+const Shop = () => {
+  return (
+    <div>
+      {SHOP_DATA.map((product) => {
+        return (
+          <div id={product.id}>
+            <h1>{product.name}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+export default Shop;
+// class ShopPage extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+//     this.state = {
+//       collections: SHOP_DATA,
+//     };
+//   }
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+//   render() {
+//     const { collections } = this.state;
+//     return (
+//       <div className='shop-page'>
+//         {collections.map(({ id, ...otherCollectionProps }) => (
+//           <CollectionPreview key={id} {...otherCollectionProps} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
