8f3277a2d3cd4e384ac16af2de331b59826bdc09
Roman Medvedev <roman.medvedev77@gmail.com>
2022-04-13 12:10:15 -0400

    change menu-items styled components

--- src/components/menu-item/Menu-item.jsx
+++ src/components/menu-item/Menu-item.jsx
@@ -1,38 +1,21 @@
 // import React from "react";
-import "./Menu-item.scss";
-// import { withRouter } from "react-router-dom";
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from "./Menu-item.style.jsx";
 
 const MenuItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
-    <div className='menu-item'>
-      <div
-        className='background-image'
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
-      <div className='content'>
-        <h2 className='title'>{title}</h2>
+    <DirectoryItemContainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
+        <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
-
-    // <div
-    //   className={`${size} menu-item`}
-    //   onClick={() => history.push(`${match.url}${linkUrl}`)}
-    // >
-    // <div
-    //   className='background-image'
-    //   style={{ backgroundImage: `url(${imageUrl})` }}
-    // />
-    // <div className='content'>
-    //   <h1 className='title'>{title.toUpperCase()}</h1>
-    //   <span className='subtitle'>SHOP NOW</span>
-    // </div>
-    // </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 };
 
-// export default withRouter(MenuItem);
 export default MenuItem;
