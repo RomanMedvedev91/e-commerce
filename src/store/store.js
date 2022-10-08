1014537f8c3cf2b382b425c042811e160b2fd15c
Roman Medvedev <roman.medvedev77@gmail.com>
2022-10-08 09:54:44 -0400

    add user reducer with redux, clean up code

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { compose, createStore, appyMiddleware, applyMiddleware } from "redux";
+import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
@@ -7,4 +7,4 @@ const middleWares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, middleWares);
+export const store = createStore(rootReducer, undefined, composedEnhancers);
