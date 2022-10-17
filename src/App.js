3728002c68fb53501acba75701d62ae9ec5fdd93
Roman Medvedev <roman.medvedev77@gmail.com>
2022-10-16 20:11:36 -0400

    add check user session saga

--- src/App.js
+++ src/App.js
@@ -9,21 +9,13 @@ import Shop from "./routes/shop/Shop.jsx";
 import Authentication from "./components/authentication/authentication";
 
 import Checkout from "./routes/checkout/Checkout";
-import { setCurrentUser } from "./store/user/user.action.js";
-// import { setCategoriesMap } from "./store/category/category.action.js";
-
-import {
-  // onAuthStateChangedListener,
-  // createUserDocumentFromAuth,
-  getCurrentUser,
-  // getCategoriesAndDocuments,
-} from "./utils/firebase/firebase.utils.js";
+import { checkUserSession } from "./store/user/user.action.js";
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getCurrentUser().then((user) => console.log(user));
+    dispatch(checkUserSession());
   }, []);
 
   return (
